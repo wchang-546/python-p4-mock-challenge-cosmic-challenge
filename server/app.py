10d5aa6131f455f011400a410e96cf474980feb6
Will Chang <william.chaang@gmail.com>
2023-08-23 17:27:17 -0600

    Mock completed.

--- server/app.py
+++ server/app.py
@@ -20,11 +20,109 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
+class Scientists(Resource): 
+    def get(self):
+        scientists = [scientist.to_dict(rules=('-missions', '-planets')) for scientist in Scientist.query.all()]
+        
+        return make_response(scientists, 200)
+
+    def post(self): 
+        try: 
+            data = request.get_json()
+            new_scientist = Scientist(
+                name = data['name'],
+                field_of_study = data['field_of_study']
+            )
+            db.session.add(new_scientist)
+            db.session.commit()
+
+            return make_response(new_scientist.to_dict(rules=('-missions',)), 201)
+
+        except: 
+            return make_response({"errors" : ["validation errors"]}, 400)
+    
+ 
+
+class ScientistById(Resource):
+    def get(self, id):
+        scientist = Scientist.query.filter(Scientist.id == id).one_or_none()
+
+        if scientist is None:
+            return make_response({'error': 'Scientist not found'}, 404)
+
+        return make_response(scientist.to_dict(), 200)
+    
+    def patch(self, id): 
+        scientist = Scientist.query.filter(Scientist.id == id).one_or_none()
+        if scientist is None: 
+            return make_response({"error": "Scientist not found"}, 404)
+        data = request.get_json()
+        try: 
+            for key in data: 
+                setattr(scientist, key, data[key])
+                
+            db.session.add(scientist)
+            db.session.commit()
+            return make_response(scientist.to_dict(rules=('-mission', '-planets',)), 202)
+
+        except: 
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+    def delete(self, id):
+        scientist = Scientist.query.filter(Scientist.id == id).one_or_none()
+        if scientist is None: 
+            return make_response({"error": "Scientist not found"}, 404)
+        if scientist: 
+            db.session.delete(scientist)
+            db.session.commit()
+
+            return make_response({}, 204)
+
+class Planets(Resource): 
+    def get(self): 
+        planets = [planet.to_dict(rules=("-missions", "-scientists",)) for planet in Planet.query.all()]
+        return make_response(planets, 200)
+    
+    def post(self): 
+        try: 
+            data = request.get_json()
+            new_planet = Planet(
+                name = data['name'],
+                distance_from_earth = data['distance_from_earth'],
+                nearest_star = data['nearest_star']
+            )
+            db.session.add(new_planet)
+            db.session.commit()
+            return make_response(new_planet.to_dict(rules=('-missions', '-scientists')), 200)
+        except ValueError: 
+            return make_response({"errors": ["validation errors"]})
+
+class Missions(Resource): 
+    def post(self): 
+        try: 
+            data = request.get_json()
+            new_mission = Mission(
+                name = data["name"],
+                scientist_id = data["scientist_id"],
+                planet_id = data['planet_id']
+            )
+            db.session.add(new_mission)
+            db.session.commit()
+            return make_response(new_mission.to_dict(), 201)
+        except ValueError: 
+            return make_response({"errors": ["validation errors"]}, 400)
+
+api.add_resource(Missions, "/missions")
+api.add_resource(Planets, "/planets")
+api.add_resource(Scientists, "/scientists")
+api.add_resource(ScientistById, "/scientists/<int:id>")
+
 
 @app.route('/')
 def home():
     return ''
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -26,9 +26,9 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
-
+    missions = db.relationship('Mission', cascade='all, delete-orphan', backref='planet')
     # Add serialization rules
-
+    serialize_rules = ("-missions.planet",)
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
@@ -38,11 +38,21 @@ class Scientist(db.Model, SerializerMixin):
     field_of_study = db.Column(db.String)
 
     # Add relationship
-
+    missions = db.relationship('Mission', cascade='all, delete-orphan', backref='scientist')
     # Add serialization rules
-
+    serialize_rules = ("-missions.scientist", "-planets.scientists")
     # Add validation
-
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name or len(name) < 1: 
+            raise ValueError("Must have name")
+        return name 
+    
+    @validates('field_of_study')
+    def validate_field_of_study(self, key, field_of_study):
+        if not field_of_study or len(field_of_study) < 1: 
+            raise ValueError('Scientist must have field of study.')
+        return field_of_study 
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
@@ -51,10 +61,27 @@ class Mission(db.Model, SerializerMixin):
     name = db.Column(db.String)
 
     # Add relationships
-
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id')) 
     # Add serialization rules
-
+    serialize_rules = ("-scientist.missions", "-planet.missions")
     # Add validation
+    @validates('name')
+    def validate_name(self, key, name): 
+        if not name or len(name) < 1: 
+            raise ValueError('Mission must have name.')
+        return name 
+    
+    @validates('scientist_id')
+    def validate_scientist(self, key, scientist_id):
+        if scientist_id is not None:
+            return scientist_id
+        raise ValueError('Mission must have scientist ID.')
 
+    @validates('planet_id')
+    def validate_planet(self, key, planet_id):
+        if planet_id is not None:
+            return planet_id
+        raise ValueError('Mission must have planet ID.')
 
 # add any models you may need.
